aa90012cb5d419d05a7548dd2961293197d4fa4d
Anshu Kumar <anshu.builds@gmail.com>
2026-07-17 01:34:37 +0530

    Add Footer component and integrate it into App structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Reviews from './components/Reviews/Reviews'
 import About from './components/About/About'
 import Features from './components/Features/Features'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <About />
       <Features/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-around items-center font-mono text-center p-10">
+      <div className="w-100 flex flex-col items-center">
+        <img src="/src/assets/logo.jpeg" className="rounded-full h-30 w-40 my-5" alt="logo" />
+        <div className="text-blue-800 font-mono">
+          <p>
+            <span className="font-bold text-blue-900">Address : </span>
+            Thane(West), 400610
+          </p>
+          <p>
+            <span className="font-bold text-blue-900">Phone : </span>+91 98765
+            43210 / +91 98765 43210
+          </p>
+          <p>
+            <span className="font-bold text-blue-900">Email : </span>
+            xyz123@abc.com
+          </p>
+        </div>
+      </div>
+      <div className='w-100'>
+        <h4 className="text-2xl text-center mb-2 text-blue-900 font-bold">
+          About Us
+        </h4>
+        <p className="text-justify text-blue-800">
+          Welcome to Balaji Repair & Services, where innovation meets excellence. We are a passionate team dedicated to providing top-notch Washing Machine, Microwave, Fridge, AC Repair. Our mission is to deliver high-quality solutions that cater to your needs, ensuring efficiency, reliability and satisfaction. With a team of skilled professionals and years of experience, we strive to stay ahead of industry.
+        </p>
+      </div>
+      <div className='w-75'>
+        <h4 className="text-2xl text-center mb-2 text-blue-900 font-bold">
+           Quick Links
+        </h4>
+        <ul className="text-blue-800">
+          <li>Washing Machine Service</li>
+          <li>Microwave Service</li>
+          <li>AC Service</li>
+          <li>Fridge Service</li>
+          <li>Other Services & Queries</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
