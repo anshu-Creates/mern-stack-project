8573fe857e07b69f9b9a1e9337393ae36a10c0ff
Anshu Kumar <anshu.builds@gmail.com>
2026-07-16 23:53:41 +0530

    Add Contact component and integrate it into App structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './components/Home/Home'
 import Services from './components/Services/Services'
 import Reviews from './components/Reviews/Reviews'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Home/>
       <Services/>
       <Reviews/>
+      <Contact/>
     </div>
   )
 }
---
+++ Frontend/src/components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../Header'
+
+const Contact = () => {
+  return (
+    <div className='bg-white px-5 py-10'>
+        <Header h1='CONTACT' h2='US'/>
+        <div className='flex justify-around font-mono'>
+            <img src="/src/assets/f1.png" alt="Contact" className='mx-10 h-50 w-80'/>
+            <div className='px-25 w-4xl'>
+                <h4 className='text-xl text-center mb-2 text-blue-900 font-extrabold'>Who We Are ?</h4>
+                <p className='text-center'>Welcome to Balaji Repair & Services, where innovation meets excellence. We are a passionate team dedicated to providing top-notch Washing Machine, Microwave, Fridge, AC Repair. Our mission is to deliver high-quality solutions that cater to your needs, ensuring efficiency, reliability and satisfaction. With a team of skilled professionals and years of experience, we strive to stay ahead of industry.</p>
+                <h5 className='bg-blue-400 text-center my-2'>Welcome to BALAJI REPAIR AND SERVICES</h5>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
