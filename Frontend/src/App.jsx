b5927092007df8379c6a4d6f217b534f7a22e382
Anshu Kumar <anshu.builds@gmail.com>
2026-09-08 01:33:23 +0530

    Fix import paths for components to match directory structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./Pages/Homepage";
-import Loginpage from "./Pages/Loginpage";
-import Registerpage from "./Pages/Registerpage";
-import ProtectedRoute from "./Components/ProtectedRoute";
+import Homepage from "./pages/Homepage";
+import Loginpage from "./pages/Loginpage";
+import Registerpage from "./pages/Registerpage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
