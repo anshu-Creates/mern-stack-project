8a3e5ae74f7aa5b83d5adbdfe68213d4d8241d92
Anshu Kumar <anshu.builds@gmail.com>
2026-09-08 02:20:05 +0530

    Update CORS configuration to allow all origins

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors({ origin: "https://mern-stack-project-seven-lake.vercel.app/", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
